03256f859fc4d8c6c967ed177e0449654304684e
Ryan Kent <hello@interphased.com>
2014-03-21 23:26:21 -0400

    eat balls

--- game.js
+++ game.js
@@ -14,6 +14,7 @@ window.onload = function() {
         var ball_count = 0;
         var timer = 0;
         var ui;
+        var score = 0;
         var hippos;
         var hippo_pink;
         var hippo_yellow;
@@ -38,24 +39,28 @@ window.onload = function() {
             hippo_pink = game.add.sprite(game.width/2 - 40, game.height - 188, 'hippo_pink', 0);
             hippo_pink.animations.add('bite');
             hippo_pink.anchor.setTo(1, 0);
+            hippo_pink.isEating = false;
             hippos.add(hippo_pink);
 
             hippo_yellow = game.add.sprite(game.width - 188, game.height/2 - 40, 'hippo_yellow', 6);
             hippo_yellow.anchor.setTo(1, 0);
             hippo_yellow.angle = 270;
             hippo_yellow.animations.add('bite');
+            hippo_yellow.isEating = false;
             hippos.add(hippo_yellow);
 
             hippo_green = game.add.sprite(game.width/2 - 40, 0, 'hippo_green', 12);
             hippo_green.anchor.setTo(1, 0);
             hippo_green.angle = 180;
             hippo_green.animations.add('bite');
+            hippo_green.isEating = false;
             hippos.add(hippo_green);
 
             hippo_blue = game.add.sprite(0, game.height - 272 - 80, 'hippo_blue', 18);
             hippo_blue.anchor.setTo(1, 0);
             hippo_blue.angle = 90;
             hippo_blue.animations.add('bite');
+            hippo_blue.isEating = false;
             hippos.add(hippo_blue);
             
             hippos.z = 2;
@@ -80,7 +85,7 @@ window.onload = function() {
 
             // Set UI
             ui = game.add.text(game.world.centerX + 64, game.world.height - 24, "0 Balls", {
-                font: "18px slkscr",
+                font: "24px slkscr",
                 fill: "#ffffff",
                 align: "center"
             });
@@ -94,7 +99,7 @@ window.onload = function() {
 
             ball.body.velocity.setTo((Math.random() - 0.5) * 600, (Math.random() - 0.5) * 600);
             ball.body.collideWorldBounds = true;
-            ball.body.bounce.setTo(0.8, 0.8);
+            ball.body.bounce.setTo(1, 1);
 
             balls.add(ball);
 
@@ -108,25 +113,78 @@ window.onload = function() {
             game.physics.arcade.collide(balls, hippo_green, collisionHandler, null, this);
             game.physics.arcade.collide(balls, hippo_blue, collisionHandler, null, this);
 
-            if (ball_count < 100) {
+            if (ball_count < 10) {
                 Ball();
             }
 
-            if (game.input.mousePointer.isDown)
-            {
+            if (game.input.mousePointer.isDown) {
+                hippo_pink.isEating = true;   
+            }
+            else {
+                hippo_pink.isEating = false;
+            }
+            
+            eatingAnimation();
+
+        }
+
+        function eatingAnimation() {
+            if (hippo_pink.isEating) {
                 hippo_pink.animations.play('bite', 40, true);
             }
             else {
                 hippo_pink.animations.stop();
             }
-
+            if (hippo_yellow.isEating) {
+                hippo_yellow.animations.play('bite', 40, true);
+            }
+            else {
+                hippo_yellow.animations.stop();
+            }
+            if (hippo_green.isEating) {
+                hippo_green.animations.play('bite', 40, true);
+            }
+            else {
+                hippo_green.animations.stop();
+            }
+            if (hippo_blue.isEating) {
+                hippo_blue.animations.play('bite', 40, true);
+            }
+            else {
+                hippo_blue.animations.stop();
+            }
         }
 
         function collisionHandler (obj1, obj2) {
 
-            game.stage.backgroundColor = '#992d2d';
+            if (hippo_pink.isEating && obj1.key == 'hippo_pink') {
+                score += 1;
+                ui.setText(score + " Balls");
+                obj2.exists = false;
+            }
+            else if (hippo_yellow.isEating && obj1.key == 'hippo_yellow') {
+                score += 1;
+                ui.setText(score + " Balls");
+                obj2.exists = false;
+            }
+            else if (hippo_green.isEating && obj1.key == 'hippo_green') {
+                score += 1;
+                ui.setText(score + " Balls");
+                obj2.exists = false;
+            }
+            else if (hippo_blue.isEating && obj1.key == 'hippo_blue') {
+                score += 1;
+                ui.setText(score + " Balls");
+                obj2.exists = false;
+            }
 
         }
+
+        // function processHandler (obj1, obj2) {
+
+        //     game.stage.backgroundColor = '#992d2d';
+
+        // }
 
         function render() {
             // game.debug.body(hippo_pink);
