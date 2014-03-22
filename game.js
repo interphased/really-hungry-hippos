b8d3d8ff44bc4d93722b7cb20b552c26021f89e7
Ryan Kent <hello@interphased.com>
2014-03-21 21:36:23 -0400

    grouping + spawn balls in middle

--- game.js
+++ game.js
@@ -10,9 +10,11 @@ window.onload = function() {
             game.load.image('ball', 'images/ball.png');
         }
 
+        var balls;
         var ball_count = 0;
         var timer = 0;
         var ui;
+        var hippos;
         var hippo_pink;
         var hippo_yellow;
         var hippo_green;
@@ -26,26 +28,36 @@ window.onload = function() {
             game.stage.backgroundColor = '#c3574c';
             
             // Set balls
+            balls = game.add.group();
+            balls.z = 1;
             Ball();
 
             // Set sprites
+            hippos = game.add.group();
+            
             hippo_pink = game.add.sprite(game.width/2 - 64, game.height - 272, 'hippo_pink', 0);
             hippo_pink.animations.add('bite');
+            hippos.add(hippo_pink);
 
             hippo_yellow = game.add.sprite(game.width - 272, game.height/2 - 64, 'hippo_yellow', 6);
             hippo_yellow.anchor.setTo(1, 0);
             hippo_yellow.angle = 270;
             hippo_yellow.animations.add('bite');
+            hippos.add(hippo_yellow);
 
             hippo_green = game.add.sprite(game.width/2 - 64, 272, 'hippo_green', 12);
             hippo_green.anchor.setTo(1, 0);
             hippo_green.angle = 180;
             hippo_green.animations.add('bite');
+            hippos.add(hippo_green);
 
             hippo_blue = game.add.sprite(272, game.height - 272, 'hippo_blue', 18);
             hippo_blue.anchor.setTo(1, 0);
             hippo_blue.angle = 90;
             hippo_blue.animations.add('bite');
+            hippos.add(hippo_blue);
+            hippos.z = 2;
+
 
             // Set UI
             ui = game.add.text(game.world.centerX + 64, game.world.height - 24, "0 Balls", {
@@ -53,32 +65,32 @@ window.onload = function() {
                 fill: "#ffffff",
                 align: "center"
             });
-
             ui.anchor.setTo(0, 0);
         }
 
         function Ball() {
-            var ball = game.add.sprite(game.world.randomX, game.world.randomY, 'ball');
+            var ball = game.add.sprite((Math.random() * 100) + game.world.width/2 - 50, (Math.random() * 100) + game.world.height/2 - 50, 'ball');
             
             game.physics.enable([ball], Phaser.Physics.ARCADE);
 
-            ball.body.velocity.setTo((Math.random() + 10) * 40, (Math.random() + 10) * 40);
+            ball.body.velocity.setTo((Math.random() - 0.5) * 600, (Math.random() - 0.5) * 600);
             ball.body.collideWorldBounds = true;
-            ball.body.bounce.setTo(1, 1);
+            ball.body.bounce.setTo(0.8, 0.8);
+
+            balls.add(ball);
 
             ball_count++;
-            timer = game.time.now + 100;
         }
 
         function update () {
 
-            if (ball_count < 50) {
+            if (ball_count < 100) {
                 Ball();
             }
 
             if (game.input.mousePointer.isDown)
             {
-                hippo_pink.animations.play('bite', 30, true);
+                hippo_pink.animations.play('bite', 40, true);
             }
             else {
                 hippo_pink.animations.stop();
